5e15ac5094bba01955aff1d733657ac915d58b78
Tim Jacobs <timjacobs2@gmail.com>
2013-10-07 15:20:08 -0400

    update convert docs script to remove non utf characters from HTML

--- convert_docs.sh
+++ convert_docs.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env sh
+
+if [ -z "$1" ]; then
+	echo "Please provide a path to the desired gollum"
+	exit
+fi
+
+if [ ! -d "$1" ]; then
+	echo "The directory specified for the new git repository does not exist. Please create it first"
+	exit
+fi
+
 # Clean out the existing docs.
 #rm -rf html
 #rm -rf markdown
@@ -10,9 +21,11 @@
 FILES=$(find html -type f -name *.html)
 for f in $FILES
 do
-	markdown_name=`echo ${f} | cut -d'.' -f1 | sed s/html/markdown/`
-	echo $markdown_name	
-	pandoc -f html -t markdown_github -o ${markdown_name}.md ${f}
+	basename=`echo ${f} | cut -d'.' -f1 | sed s/html//`
+	markdown_name="$1$basename.md"
+	echo "converting file: ${f}"
+	iconv -f utf8 -t utf8 -c ${f} > ${f}.clean
+	pandoc -f html -t markdown_github -o ${markdown_name} ${f}.clean
 done
 
 #for i in `ls *.html | cut -d'.' -f1`; do pandoc -f html -t markdown_github -o ${i}.md ${i}.mediawiki
